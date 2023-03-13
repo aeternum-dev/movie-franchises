e2f3b1985d1afb60e56c13c3e70b82e61b7c4262
Terjék Béla Szabolcs <terjekszabolcs02@gmail.com>
2023-03-13 08:59:56 +0100

    swap hardcoded elements to state managed elements

--- src/Dropdown.tsx
+++ src/Dropdown.tsx
@@ -1,30 +1,39 @@
 import React, {useState} from 'react'
-import { render } from 'react-dom';
+import { render } from 'react-dom'
+
+import data from './FranchiseData.json'
 
 
 import "./Dropdown.css"
 
+
+
+function DropdownElements() {
+    return (
+        <div>
+        {data.franchises.map((prop : any) => (
+            <div className="DropdownBtn">
+              <h1 className="Title">{prop.title}</h1>
+              <div className="imageContainer"><img src={prop.backdrop} alt=""/></div>
+            </div>
+          ))}
+        </div>
+        );
+}
+
 function Dropdown() {
     const [open, setopen] = useState(false)
+
+    
 
     return (
         <div className='DropdownBtn' onClick={() => setopen(!open)}>
             <h1 className='Title'>Star Wars</h1>
-            <div className="imageContainer"><img src="../src/assets/sw-bg-img.jpg" alt=""></img></div>
+            <div className="imageContainer"><img src="../src/assets/sw-bg-img.jpg" alt=""/></div>
             {open && (<div className='DropdownMenu'>
-                <div className="DropdownBtn">
-                    <h1 className="Title">Lord of the Rings</h1>
-                    <div className="imageContainer"><img src="../src/assets/lotr-bg-img.jpg" alt=""></img></div>
-                </div>
-                <div className="DropdownBtn">
-                    <h1 className="Title">Planet of the Apes</h1>
-                    <div className="imageContainer"><img src="../src/assets/poa-bg-img.jpg" alt=""></img></div>
-                </div>
-                <div className="DropdownBtn">
-                    <h1 className="Title">Godfather</h1>
-                    <div className="imageContainer"><img src="../src/assets/gf-bg-img.jpg" alt=""></img></div>
-                </div>
-                
+                {
+                   DropdownElements()         
+        }
             </div>) }
 
         </div>
